2d141373f5f42f5144c4cee87dd3b7013e052bf1
Jesse McFarland <jesse.m.mcfarland3@gmail.com>
2016-12-14 09:44:18 -0500

    Day 6 Part 2

---
+++ 2016/day6/b.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "fmt"
+    "bufio"
+    "os"
+    "log"
+    "strings"
+)
+
+type CharacterCount struct {
+    character string
+    count int
+}
+
+func getUniqueCharacters(s string) []string {
+    m := make(map[rune]uint, len(s))
+    unique := ""
+    for _, r := range s {
+        m[r]++
+        if m[r] == 1 {
+            unique = unique + string(r)
+        }
+    }
+    return strings.Split(unique, "")
+}
+
+func main() {
+    inputFile, err := os.Open("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer inputFile.Close()
+
+    var rows []string
+    scanner := bufio.NewScanner(inputFile)
+    for scanner.Scan() {
+        rows = append(rows, strings.TrimSpace(scanner.Text()))
+    }
+
+    streams := make(map[int]string)
+    for _, row := range rows {
+        columns := strings.Split(row, "")
+        for column, value := range columns {
+            streams[column] = streams[column] + value
+        } 
+    }
+
+    message := make([]string, len(streams))
+    for stream := 0; stream < len(streams); stream++ {
+        uniqueCharacters := getUniqueCharacters(streams[stream])
+        characterCount := CharacterCount{streams[stream][:1], len(streams[stream])}
+        for _, character := range uniqueCharacters {
+            count := strings.Count(streams[stream], character)
+            if count < characterCount.count {
+                characterCount.character = character
+                characterCount.count = count
+                message[stream] = characterCount.character 
+            }
+        }
+    }
+
+    fmt.Println(strings.Join(message, ""))
+}
